747cbe79f11c4f7ed25373cbf0af3c73408716d8
Md. Shadab Hussain <shadab.iiitnr20@gmail.com>
2023-10-24 14:00:01 +0530

    Infinite-Scroll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
+  const [dataSource, setDataSource] = useState(Array.from({ length: 20 }));
+  const [hasMore, setHasMore] = useState(true);
+
+  const handleMoreData = () => {
+    if (dataSource.length < 100) {
+      setTimeout(() => {
+        setDataSource(dataSource.concat(Array.from({ length: 10 })));
+      }, 500);
+    } else {
+      setHasMore(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-4">
+      <h2 className="text-xl underline font-semibold m-3">Infinite Scroll</h2>
+      <InfiniteScroll
+        dataLength={dataSource.length}
+        hasMore={hasMore}
+        next={handleMoreData}
+        loader={<p>Loading..</p>}
+        endMessage={<p>You are all set..</p>}
+      >
+        {dataSource.map((item, i) => {
+          return (
+            <div
+              className="border border-solid border-black p-1 m-2 w-1/6 rounded-lg shadow-lg"
+              key={i}
+            >
+              This is {i + 1} item
+            </div>
+          );
+        })}
+      </InfiniteScroll>
     </div>
   );
 }
